db22048c1f1a0460a8f27b602fa27f777561a32f
albertarrebola08 <albertarrebola8@gmail.com>
2024-02-22 11:44:48 +0100

    delete questions ok, dynamic ok, reordered ok

--- src/components/Examenes/ExamenForm.jsx
+++ src/components/Examenes/ExamenForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../../supabase/supabaseClient";
-import { Button, IconButton, Input, Checkbox } from "pol-ui";
+import { Button, IconButton, Input, Checkbox, Loader } from "pol-ui";
 
 import {
   Carousel,
@@ -18,6 +18,7 @@ const ExamenForm = () => {
     preguntas: [],
   });
   const { elementoId } = useParams();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchExamen = async () => {
@@ -43,7 +44,73 @@ const ExamenForm = () => {
     fetchExamen();
   }, [elementoId]);
 
-  const handleAgregarPregunta = () => {
+  const handleDeleteQuestion = async (preguntaId) => {
+    try {
+      setLoading(true);
+      await handleSubmit();
+      // Obtener el examen de la base de datos
+      const { data, error } = await supabase
+        .from("examen")
+        .select("*") // Seleccionar todas las columnas, incluyendo el desarrollo
+        .eq("elemento_id", parseInt(elementoId))
+        .single();
+
+      if (error) {
+        console.error("Error al obtener el examen:", error.message);
+        return;
+      }
+
+      // Obtener el campo desarrollo del examen
+      const desarrollo = data.desarrollo || { preguntas: [] };
+
+      // Filtrar y reorganizar las preguntas
+      const nuevasPreguntas = desarrollo.preguntas
+        .filter((p) => p.id !== preguntaId)
+        .map((p, index) => ({
+          ...p,
+          id: index + 1, // Reasignar el ID de la pregunta
+        }));
+
+      // Actualizar el examen en la base de datos
+      const { data: updatedData, error: updateError } = await supabase
+        .from("examen")
+        .update({ desarrollo: { preguntas: nuevasPreguntas } })
+        .eq("elemento_id", parseInt(elementoId))
+        .single();
+
+      if (updateError) {
+        console.error("Error al actualizar el examen:", updateError.message);
+        return;
+      }
+
+      console.log("Examen actualizado:", updatedData);
+
+      // Volver a obtener los datos actualizados del examen desde la base de datos
+      const { data: updatedExamenData, error: updatedExamenError } =
+        await supabase
+          .from("examen")
+          .select("*")
+          .eq("elemento_id", parseInt(elementoId))
+          .single();
+
+      if (updatedExamenError) {
+        console.error(
+          "Error al obtener los datos actualizados del examen:",
+          updatedExamenError.message
+        );
+        return;
+      }
+
+      // Actualizar el estado local con los datos actualizados del examen
+      setExamen(updatedExamenData?.desarrollo ?? { preguntas: [] });
+    } catch (error) {
+      console.error("Error al eliminar la pregunta:", error.message);
+    } finally {
+      setLoading(false); // Desactivar indicador de carga
+    }
+  };
+
+  const handleAddQuestion = () => {
     const nuevaPregunta = {
       id: examen.preguntas.length + 1,
       enunciado: "",
@@ -55,7 +122,7 @@ const ExamenForm = () => {
     }));
   };
 
-  const handleModificarEnunciado = (preguntaId, enunciado) => {
+  const handleModifyEnun = (preguntaId, enunciado) => {
     setExamen((prevState) => ({
       ...prevState,
       preguntas: prevState.preguntas.map((pregunta) =>
@@ -66,7 +133,7 @@ const ExamenForm = () => {
     }));
   };
 
-  const handleAgregarRespuesta = (preguntaId) => {
+  const handleAddAnswer = (preguntaId) => {
     setExamen((prevState) => ({
       ...prevState,
       preguntas: prevState.preguntas.map((pregunta) =>
@@ -87,7 +154,7 @@ const ExamenForm = () => {
     }));
   };
 
-  const handleModificarRespuesta = (preguntaId, respuestaId, texto) => {
+  const handleModifyAnswer = (preguntaId, respuestaId, texto) => {
     setExamen((prevState) => ({
       ...prevState,
       preguntas: prevState.preguntas.map((pregunta) =>
@@ -105,7 +172,7 @@ const ExamenForm = () => {
     }));
   };
 
-  const handleModificarCorrecta = (preguntaId, respuestaId) => {
+  const handleModifyCorrect = (preguntaId, respuestaId) => {
     setExamen((prevState) => ({
       ...prevState,
       preguntas: prevState.preguntas.map((pregunta) =>
@@ -123,7 +190,7 @@ const ExamenForm = () => {
       ),
     }));
   };
-  const handleModificarNumeric = (preguntaId, respuestaId) => {
+  const handleModifyNumeric = (preguntaId, respuestaId) => {
     setExamen((prevState) => ({
       ...prevState,
       preguntas: prevState.preguntas.map((pregunta) =>
@@ -141,7 +208,7 @@ const ExamenForm = () => {
     }));
   };
 
-  const handleEliminarRespuesta = (preguntaId, respuestaId) => {
+  const handleDeleteAnswer = (preguntaId, respuestaId) => {
     setExamen((prevState) => ({
       ...prevState,
       preguntas: prevState.preguntas.map((pregunta) =>
@@ -168,7 +235,7 @@ const ExamenForm = () => {
       console.log("Examen guardado en la base de datos");
 
       // Actualizar el estado del examen con el examen guardado
-      setExamen((prevState) => ({ ...prevState, preguntas: [] }));
+      setExamen({ ...examen });
     } catch (error) {
       console.error(
         "Error al guardar el examen en la base de datos:",
@@ -179,6 +246,7 @@ const ExamenForm = () => {
 
   return (
     <div className="flex flex-col gap-3 pl-5">
+      <div>{loading && <Loader />}</div>
       <Carousel
         options={{ startIndex: examen.preguntas.length }}
         className="max-w-[600px]"
@@ -189,7 +257,11 @@ const ExamenForm = () => {
               <div className="bg-primary-400 rounded-xl p-3 flex flex-col gap-2">
                 <div className="flex gap-2 items-center">
                   <h3 className="font-bold">Pregunta {pregunta.id}</h3>
-                  <IconButton color="error" className="pb-1">
+                  <IconButton
+                    color="error"
+                    className="pb-1"
+                    onClick={() => handleDeleteQuestion(pregunta.id)}
+                  >
                     <RiDeleteBin3Fill />
                   </IconButton>
                 </div>
@@ -198,14 +270,14 @@ const ExamenForm = () => {
                   type="text"
                   value={pregunta.enunciado}
                   onChange={(e) =>
-                    handleModificarEnunciado(pregunta.id, e.target.value)
+                    handleModifyEnun(pregunta.id, e.target.value)
                   }
                   placeholder="Ingrese el enunciado"
                 />
                 <h4>Respuestas</h4>
                 <IconButton
                   color="warning"
-                  onClick={() => handleAgregarRespuesta(pregunta.id)}
+                  onClick={() => handleAddAnswer(pregunta.id)}
                 >
                   <RiAddFill></RiAddFill>
                 </IconButton>
@@ -216,7 +288,7 @@ const ExamenForm = () => {
                       className="py-2"
                       value={respuesta.texto}
                       onChange={(e) =>
-                        handleModificarRespuesta(
+                        handleModifyAnswer(
                           pregunta.id,
                           respuesta.id,
                           e.target.value
@@ -228,20 +300,20 @@ const ExamenForm = () => {
                       label="Correcta? "
                       checked={respuesta.esCorrecta}
                       onChange={() =>
-                        handleModificarCorrecta(pregunta.id, respuesta.id)
+                        handleModifyCorrect(pregunta.id, respuesta.id)
                       }
                     />
                     <Checkbox
                       label="Numeral? "
                       checked={respuesta.esNumeral}
                       onChange={() =>
-                        handleModificarNumeric(pregunta.id, respuesta.id)
+                        handleModifyNumeric(pregunta.id, respuesta.id)
                       }
                     />
                     <IconButton
                       color="error"
                       onClick={() =>
-                        handleEliminarRespuesta(pregunta.id, respuesta.id)
+                        handleDeleteAnswer(pregunta.id, respuesta.id)
                       }
                     >
                       <RiDeleteBin3Fill />
@@ -252,10 +324,10 @@ const ExamenForm = () => {
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        {examen.preguntas.length > 0 && <CarouselPrevious />}
+        {examen.preguntas.length > 0 && <CarouselNext />}
       </Carousel>
-      <Button className="w-fit bg-primary-900" onClick={handleAgregarPregunta}>
+      <Button className="w-fit bg-primary-900" onClick={handleAddQuestion}>
         <RiAddFill></RiAddFill>
       </Button>
       <Button className=" bg-success-700 mb-6" onClick={handleSubmit}>
