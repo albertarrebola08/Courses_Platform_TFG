6bf1983236b7694c66ffe717d34165b1a8503f92
albertarrebola08 <albertarrebola8@gmail.com>
2024-03-10 17:21:41 +0100

    added some sidebar items layout dashbpard admin

--- src/components/Layout/SidebarDefault.jsx
+++ src/components/Layout/SidebarDefault.jsx
@@ -1,6 +1,20 @@
-import { Sidebar, SidebarItem } from "pol-ui";
+import { Sidebar, SidebarCollapse, SidebarItem } from "pol-ui";
 import React, { useState } from "react";
-import { RiQuestionFill } from "react-icons/ri";
+import { LuDices } from "react-icons/lu";
+import { MdBackHand } from "react-icons/md";
+import {
+  Ri4KFill,
+  RiBookOpenFill,
+  RiCheckFill,
+  RiFileEditFill,
+  RiFolderKeyholeFill,
+  RiListCheck3,
+  RiLogoutBoxFill,
+  RiQuestionFill,
+  RiUser3Fill,
+  RiUserSettingsFill,
+  RiVideoFill,
+} from "react-icons/ri";
 import { TiHome } from "react-icons/ti";
 
 const SidebarDefault = () => {
@@ -28,6 +42,51 @@ const SidebarDefault = () => {
         icon={RiQuestionFill}
       >
         Preguntes diaries
+      </SidebarItem>
+      <SidebarCollapse icon={RiFolderKeyholeFill} label="Repositori">
+        <SidebarItem icon={RiVideoFill} href="#">
+          Videos
+        </SidebarItem>
+        <SidebarItem icon={RiBookOpenFill} href="#">
+          Materials
+        </SidebarItem>
+
+        <SidebarItem icon={MdBackHand} href="#">
+          Accionas
+        </SidebarItem>
+        <SidebarItem icon={RiFileEditFill} href="#">
+          Exàmens
+        </SidebarItem>
+        <SidebarCollapse label="Activitats" href="#" className="">
+          <SidebarItem icon={RiListCheck3} href="#">
+            Quizs
+          </SidebarItem>
+          <SidebarItem icon={LuDices} href="#">
+            Numerals
+          </SidebarItem>
+        </SidebarCollapse>
+      </SidebarCollapse>
+      <SidebarItem
+        className="bg-white-600 hover:bg-primary-200"
+        icon={RiUser3Fill}
+        href="/dashboard/cursos"
+      >
+        Usuaris
+      </SidebarItem>
+      <hr />
+      <SidebarItem
+        className="bg-white-600 hover:bg-primary-200"
+        icon={RiUserSettingsFill}
+        href="/dashboard/cursos"
+      >
+        El meu perfil
+      </SidebarItem>
+      <SidebarItem
+        className="bg-white-600 hover:bg-primary-200"
+        icon={RiLogoutBoxFill}
+        href="/dashboard/cursos"
+      >
+        Tancar sesió
       </SidebarItem>
     </Sidebar>
   );
